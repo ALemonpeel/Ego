f13898d416c6c0549899a98da1b8936d15bccf0b
lemonpeel <lemonpeel@aliyun.com>
2023-08-25 21:09:01 +0800

    打包，解决跨域问题

--- vue-purhase/src/api/base.js
+++ vue-purhase/src/api/base.js
@@ -1,33 +1,34 @@
 //请求接口的配置
+//npm run serve 时将base中的http://127.0.0.1:9898改为api
 const base = {
   host: 'http://127.0.0.1:9898',
-  homeCount: '/api/home/dataCount',//首页的数据统计接口
-  homeFormat: '/api/home/format',//首页折线图
-  homeOrder: '/api/home/orderinfo',//首页今日订单
+  homeCount: 'http://127.0.0.1:9898/home/dataCount',//首页的数据统计接口
+  homeFormat: 'http://127.0.0.1:9898/home/format',//首页折线图
+  homeOrder: 'http://127.0.0.1:9898/home/orderinfo',//首页今日订单
   //产品管理接口
-  goodsList: '/api/goods/projectList',//商品列表接口
-  goodsSearch: '/api/goods/search',//商品查询接口
-  goodsDelete: '/api/goods/deleteItemById',//删除商品接口
-  goodsClass: '/api/goods/itemCategory/selectItemCategoryByParentId', //获取类目的接口
-  addGoods: '/api/goods/item/insertTbItem',//添加商品的接口
-  updateTbItem: '/api/goods/item/updateTbItem',//更改商品的接口
-  batchDelete: '/api/goods/batchDelete',//批量删除的接口
+  goodsList: 'http://127.0.0.1:9898/goods/projectList',//商品列表接口
+  goodsSearch: 'http://127.0.0.1:9898/goods/search',//商品查询接口
+  goodsDelete: 'http://127.0.0.1:9898/goods/deleteItemById',//删除商品接口
+  goodsClass: 'http://127.0.0.1:9898/goods/itemCategory/selectItemCategoryByParentId', //获取类目的接口
+  addGoods: 'http://127.0.0.1:9898/goods/item/insertTbItem',//添加商品的接口
+  updateTbItem: 'http://127.0.0.1:9898/goods/item/updateTbItem',//更改商品的接口
+  batchDelete: 'http://127.0.0.1:9898/goods/batchDelete',//批量删除的接口
   //订单列表接口
-  orderList: '/api/order/list',  //订单列表的接口
-  changeStatus: '/api/order/changeStatus', //订单汇总接口
-  detail: '/api/order/list/detail',//订单详情接口
-  collect: '/api/order/collect', //汇总订单列表接口
-  cancel: '/api/order/cancel', //撤销汇总的接口
+  orderList: 'http://127.0.0.1:9898/order/list',  //订单列表的接口
+  changeStatus: 'http://127.0.0.1:9898/order/changeStatus', //订单汇总接口
+  detail: 'http://127.0.0.1:9898/order/list/detail',//订单详情接口
+  collect: 'http://127.0.0.1:9898/order/collect', //汇总订单列表接口
+  cancel: 'http://127.0.0.1:9898/order/cancel', //撤销汇总的接口
   //类目管理
-  itemCategory: '/api/itemCategory',//获取所有类目
-  insertCategory: '/api/itemCategory/insertCategory',//新增一级类目
-  updateCategory: '/api/itemCategory/updateCategory', //修改类目名称
-  deleteContentCategoryById: '/api/content/deleteContentCategoryById',//删除类目
-  insertItemCategory: '/api/itemCategory/insertItemCategory', //添加子类目
+  itemCategory: 'http://127.0.0.1:9898/itemCategory',//获取所有类目
+  insertCategory: 'http://127.0.0.1:9898/itemCategory/insertCategory',//新增一级类目
+  updateCategory: 'http://127.0.0.1:9898/itemCategory/updateCategory', //修改类目名称
+  deleteContentCategoryById: 'http://127.0.0.1:9898/content/deleteContentCategoryById',//删除类目
+  insertItemCategory: 'http://127.0.0.1:9898/itemCategory/insertItemCategory', //添加子类目
 
   //登录的接口
-  login: '/api/login',   //登录接口
-  permission: '/api/permission' //用户权限数据接口
+  login: 'http://127.0.0.1:9898/login',   //登录接口
+  permission: 'http://127.0.0.1:9898/permission' //用户权限数据接口
 }
 //导出单个文件
 export const host = 'http://127.0.0.1:9898/'
